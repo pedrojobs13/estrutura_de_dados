10d1804c033d7827233ef445a9fceccbef521cbf
Pedro Lucas Gomes de Oliveira <pedro.lg.oliveira@edu.ufes.br>
2022-12-14 10:35:08 -0300

    Add files via upload

--- Lista Sequencial Estática/Lista.h
+++ Lista Sequencial Estática/Lista.h
@@ -3,14 +3,25 @@
 
 #define MAX 5
 
-struct No
+struct Aluno
 {
-    int value;
-    char name[30];
-} no;
+    int matricula;
+    char nome[30];
+    float nota1;
+    float nota2;
+    float nota3;
+};
 
 typedef struct lista Lista; // mudando o nome da lista de struct lista para Lista
 
 Lista *criar_lista();
-
 void libera_lista(Lista *li);
+int tamanho_lista(Lista *li);
+int lista_cheia(Lista *li);
+int lista_vazia(Lista *li);
+int insere_lista_inicio(Lista *li, struct Aluno al);
+int insere_lista_final(Lista *li, struct Aluno al);
+int insere_lista_ordenada(Lista *li, struct Aluno al);
+int remove_lista_final(Lista *li);
+int remove_lista_inicio(Lista *li);
+int consulta_lista_pos(Lista *li, int posicao, struct Aluno *al);
--- Lista Sequencial Estática/main.c
+++ Lista Sequencial Estática/main.c
@@ -1,12 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Lista.h"
+#include <string.h>
 
 int main()
 {
-    Lista *li;
+  Lista *li;
 
-    li = criar_lista();
-    libera_lista(li);
-    return 0;
+  li = criar_lista();
+  int tam = tamanho_lista(li);
+  int posicao;
+  struct Aluno ali1;
+
+  for (int i = 0; i < MAX; i++)
+  {
+    printf("Informe a matricula:\n");
+    scanf("%d", &ali1.matricula);
+    printf("Informe o nome:\n");
+    scanf("%s", &ali1.nome);
+    printf("Informe a nota 1:\n");
+    scanf("%f", &ali1.nota1);
+  }
+
+  /*  int x = lista_cheia(li);
+      if(lista_cheia(li));
+  */
+
+  /*int x = lista_vazia(li);
+   if (lista_vazia(li))
+   ;*/
+
+  int x = insere_lista_inicio(li, ali1);
+  printf("%d", x);
+  //  int a = insere_lista_final(li, ali1);
+  // int x = insere_lista_ordenada(li, ali1);
+  //  int x = remove_lista_final(li);
+  //  int x = remove_lista_inicio(li);
+  libera_lista(li);
+  // int x = consulta_lista_pos(li, posicao, &ali1);
+  return 0;
 }
