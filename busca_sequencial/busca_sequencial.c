fa51eb57f01370431fe3d7a8729e34050f03394e
Pedro Lucas Gomes <pedro.lg.oliveira@edu.ufes.br>
2023-05-08 15:28:49 -0300

    Update busca_sequencial.c

--- busca_sequencial/busca_sequencial.c
+++ busca_sequencial/busca_sequencial.c
@@ -34,7 +34,10 @@ int main()
 int busca(struct Aluno *aluno, int n, int ele)
 {
     int i = 0;
-    // o while serve pra verificar se o i é menor que o n, pois o elemento que não vai ser encontrado será salvo no último elemento e o aluno[i].id !=  ele, pois assim ele verifica se o elemento é diferente, caso seja igual ele para o looping se for diferente ele continua e só para quando o i é menor q n, fazendo assim o elemento ser salvo no n + 1; 
+    // o while serve pra verificar se o i é menor que o n,
+    //pois o elemento que não vai ser encontrado será salvo no último elemento e o aluno[i].id !=  ele, 
+    //pois assim ele verifica se o elemento é diferente, caso seja igual ele para o looping se for diferente ele continua e só para quando o i é menor q n, 
+    //fazendo assim o elemento ser salvo no n + 1; 
     while (i < n && aluno[i].id != ele)
     {
         i++;
